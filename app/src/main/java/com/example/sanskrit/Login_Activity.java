1646d71e8cdd91ec25609f277720ad2e00f72b1d
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-20 00:21:59 +0530

    login activity commit

--- app/src/main/java/com/example/sanskrit/Login_Activity.java
+++ app/src/main/java/com/example/sanskrit/Login_Activity.java
@@ -2,7 +2,9 @@ package com.example.sanskrit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(Login_Activity.this, front.class));
+            finish();
+        }.
     }
 }
