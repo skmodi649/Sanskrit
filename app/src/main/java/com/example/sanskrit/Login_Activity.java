d8b316b8501bb45927cee5f10c751dd66359f1fb
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-21 13:16:11 +0530

    login activity commit

--- app/src/main/java/com/example/sanskrit/Login_Activity.java
+++ app/src/main/java/com/example/sanskrit/Login_Activity.java
@@ -8,6 +8,5 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
     }
 }
