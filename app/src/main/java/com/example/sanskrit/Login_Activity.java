ecd79c7767c7849027c9eba696058e4db102eaec
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-24 14:24:41 +0530

    login activity xml button commit

--- app/src/main/java/com/example/sanskrit/Login_Activity.java
+++ app/src/main/java/com/example/sanskrit/Login_Activity.java
@@ -1,12 +1,27 @@
 package com.example.sanskrit;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.LocaleList;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Activity extends AppCompatActivity {
+    Button login , signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login_Activity.this , front.class));
+            }
+        });
     }
 }
