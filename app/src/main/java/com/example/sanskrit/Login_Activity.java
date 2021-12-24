365970090727e119fdc391f6598b15dfbf5dafbb
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-24 14:41:35 +0530

    register activity commit

--- app/src/main/java/com/example/sanskrit/Login_Activity.java
+++ app/src/main/java/com/example/sanskrit/Login_Activity.java
@@ -3,12 +3,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.LocaleList;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
-    Button login , signup;
+    Button login;
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,7 @@ public class Login_Activity extends AppCompatActivity {
         login = findViewById(R.id.login);
         signup = findViewById(R.id.signup);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Login_Activity.this , front.class));
-            }
-        });
+        login.setOnClickListener(view -> startActivity(new Intent(Login_Activity.this , front.class)));
+        signup.setOnClickListener(view -> startActivity(new Intent(Login_Activity.this , Register_Activity.class)));
     }
 }
