97b4b10a1f9a1cb78eb3846fc58c21577caa3f1c
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-30 21:49:02 +0530

    Login activity java class commit

--- app/src/main/java/com/example/sanskrit/Login_Activity.java
+++ app/src/main/java/com/example/sanskrit/Login_Activity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
     Button login;
     TextView signup;
+    EditText email , password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +18,15 @@ public class Login_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         login = findViewById(R.id.login);
         signup = findViewById(R.id.signup);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        String emaila = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
 
-        login.setOnClickListener(view -> startActivity(new Intent(Login_Activity.this , front.class)));
+        if(emaila.equalsIgnoreCase(pass)) {
+
+            login.setOnClickListener(view -> startActivity(new Intent(Login_Activity.this, front.class)));
+        }
         signup.setOnClickListener(view -> startActivity(new Intent(Login_Activity.this , Register_Activity.class)));
     }
 }
