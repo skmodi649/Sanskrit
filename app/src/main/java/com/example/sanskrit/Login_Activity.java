a83fa3f4af0d495a811487838b0a4b70d0b5855f
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-31 21:11:09 +0530

    Login java class commit

--- app/src/main/java/com/example/sanskrit/Login_Activity.java
+++ app/src/main/java/com/example/sanskrit/Login_Activity.java
@@ -3,9 +3,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
     Button login;
@@ -23,10 +25,12 @@ public class Login_Activity extends AppCompatActivity {
         String emaila = email.getText().toString().trim();
         String pass = password.getText().toString().trim();
 
-        if(emaila.equalsIgnoreCase(pass)) {
-
-            login.setOnClickListener(view -> startActivity(new Intent(Login_Activity.this, front.class)));
-        }
+        login.setOnClickListener(view -> {
+            if(emaila.equalsIgnoreCase(pass))
+                startActivity(new Intent(Login_Activity.this , front.class));
+            else
+                Toast.makeText(Login_Activity.this , "Invalid Credentials!" , Toast.LENGTH_LONG).show();
+        });
         signup.setOnClickListener(view -> startActivity(new Intent(Login_Activity.this , Register_Activity.class)));
     }
 }
