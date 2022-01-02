a7e35dc64138471dc1d621c3be8cf85da0c79705
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-01-02 19:18:29 +0530

    auth

---
+++ app/src/main/java/com/example/sanskrit/Auth.java
@@ -0,0 +1,14 @@
+package com.example.sanskrit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Auth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
